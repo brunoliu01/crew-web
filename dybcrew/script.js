67d07d855234f874d76e73deb9943b0cc6ced7a0
bruno <bruno.97.daria@gmail.com>
2018-05-29 20:59:57 -0500

    agrego nuevas secciones y fotos

---
+++ dybcrew/script.js
@@ -0,0 +1,36 @@
+
+
+
+function valida() {
+	if (document.formW.nombre.value.length==0) {
+		alert("Recuerde que tiene que escribir su nombre")
+		document.formW.nombre.focus ()
+		return 0;
+
+		}
+
+	correo= document.formW.correo.value
+	if (correo=="") {
+		alert("olvidaste colocar tu correo")
+		document.formW.correo.focus()
+		return 0;
+		}
+
+	telefono = document.formW.telefono.value
+	if (telefono=="") {
+		alert("tiene que introducir el número de su teléfono.")
+		document.formW.telefono.focus()
+		return 0;
+		}
+
+	 mensaje= document.formW.mensaje.value
+	if (mensaje=="") {
+		alert("No te olvides de comentarme tu idea")
+		document.formW.mensaje.focus()
+		return 0;
+		}
+
+	alert("muchas gracias por enviar el formulario");
+	document.formW.submit();
+	}
+
